4565b6bf1bc631e21ab2f6883e23831a08ad837a
Denise Williams <williamsde@outlook.com>
2016-06-17 14:45:13 -0400

    Added menu

---
+++ SummerSchool/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Enroll a Student");
+            Console.WriteLine("2. Unenroll a student");
+            Console.WriteLine("3. Print out the list of enrolled students");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine(">");
+            Console.ReadKey();
+        }
+    }
+}
